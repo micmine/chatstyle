f9722fb6fb1227a3dfd81966588e07ce323e73c7
micmine <micmine4@gmail.com>
2020-03-18 16:51:59 +0100

    Add file copy

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const { compileSassAndSaveMultiple } = require('compile-sass');
+const {
+    compileSassAndSaveMultiple
+} = require('compile-sass');
 const fs = require('fs');
 const path = require('path');
 
 const out = 'chat.css';
 const buildPath = 'build/';
 
-if (!fs.existsSync(buildPath)){
-    fs.mkdirSync(buildPath);
-}
 
 compileSassAndSaveMultiple({
     sassPath: path.join(__dirname, 'src/'),
     cssPath: path.join(__dirname, buildPath),
     sassFileExt: 'scss',
     files: ['chat.scss']
+});
+
+fs.copyFile("build/chat.css", "chat.css", (err) => {
+    if (err) throw err;
+});
+fs.copyFile("build/chat.css", "test/chat.css", (err) => {
+    if (err) throw err;
 });
